f30c12b1178e26dcbef3900a2b6918d400ccd4c3
abdulmelikkalkan <kalkanabdulmelik@gmail.com>
2022-09-07 15:48:37 +0300

    build github action

---
+++ index.js
@@ -0,0 +1,27 @@
+const core = require("@actions/core");
+const { Octokit } = require("octokit");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+async function run() {
+  try {
+    const { data: user } = await octokit.request("GET /user");
+    console.log(`authenticated as ${user.login}`);
+    const { status: status, data: res } = await octokit.request(
+      "POST /repos/{owner}/{repo}/merge-upstream",
+      {
+        owner: core.getInput('owner'),
+        repo: core.getInput('repo'),
+        branch: core.getInput('branch'),
+      }
+    );
+    console.log("Status: " + status);
+    console.log("Message: " + res.message);
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run();
